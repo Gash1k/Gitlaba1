d047193917e0ac2b17ae8e0316016ec89b11e1df
Ron1111k <rik2005@internet.ru>
2024-03-25 09:39:54 +0700

    FF

--- LABA.py
+++ LABA.py
@@ -17,12 +17,13 @@ def calculate_sector_area(radius, angle):
 
 # Пользовательский ввод радиуса окружности и угла кругового сектора
 radius = float(input("Введите радиус окружности: "))
-angle = float(input("Введите угол кругового сектора (в градусах): "))
-
+angle = float(input("Введите угол кругового сектора ( в градусах): "))
+if 0<=angle<=360:
+    sector_area = calculate_sector_area(radius, angle)
 # Вычисление и вывод результата
 circumference = calculate_circumference(radius)
 area = calculate_area(radius)
-sector_area = calculate_sector_area(radius, angle)
+
 
 print("Длина окружности: ", circumference)
 print("Площадь окружности: ", area)
